2952adcbebc35542bae8a3b16b4009c6b3e50616
ArijitKD <arijitkrdas2004@outlook.com>
2026-02-10 15:23:26 +0530

    Indentation fix
    Signed-off-by: ArijitKD <arijitkrdas2004@outlook.com>

--- serial/macs.h
+++ serial/macs.h
@@ -5,7 +5,7 @@
 #include "port.h"
 
 #define MACS_DEVICE_NAME	"MACS001"
-#define MACS_REPLY_STR		"MACS001"
+#define MACS_REPLY_STR	"MACS001"
 
 uint_t get_macs_port_no(uint_t* active_ports_buf, uint_t active_ports_buf_size, serial_properties_t* s_properties);
 
