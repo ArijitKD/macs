6452f20b988c4ce081a59c36cafcaf32f8923410
Arijit Kumar Das <arijitkdgit.official@gmail.com>
2026-03-01 14:54:15 +0530

    Added minimal command line support just for testing at the moment.
    Signed-off-by: Arijit Kumar Das <arijitkdgit.official@gmail.com>

--- daemon.c
+++ daemon.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include "serial/port.h"
 #include "serial/io.h"
 #include "serial/macs.h"
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    uint_t status_on = 1;
+    if (argc > 1 && strcmp(argv[1], "off") == 0)
+    {
+        printf ("New state = OFF\n");
+        status_on = 0;
+    }
+    else if (argc > 1 && strcmp(argv[1], "on") == 0)
+    {
+        printf ("New state = ON\n");
+    }
+    else
+    {
+        printf ("Bad flag or no flag (only \'on\' or \'off\' accepted).\n");
+        return 1;
+    }
     serial_properties_t s_properties = { MACS_BAUDRATE, MACS_SERIAL_IO_TIMEOUT_MS };
     uint_t ports[PORT_MAX] = {0};
     uint_t active_ports_count = get_active_port_nos(ports, PORT_MAX);
@@ -13,22 +29,20 @@ int main(void)
 
     if (macs_port == -1)
     {
-        printf ("Error: No MACS device found!!!\n");
+        printf ("Error: No MACS device found!!!\nFailed to change state.\n");
         return 1;
     }
     printf ("MACS device detected at port: COM%d\n", macs_port);
 
     serial_handle_t hserial = serial_open(macs_port, &s_properties);
     int bytes_wrote;
- 
-    for (int i = 0; i < 2; ++i)
-    {
+
+    if (status_on)
         bytes_wrote = serial_write(hserial, MACS_SET_ON, MACS_ON_OFF_WRITE_SIZE);
-        printf("Bytes wrote = %d\n", bytes_wrote);
-        serial_wait(1000);
+    else
         bytes_wrote = serial_write(hserial, MACS_SET_OFF, MACS_ON_OFF_WRITE_SIZE);
-        printf("Bytes wrote = %d\n", bytes_wrote);
-    }
+
+    printf("Bytes wrote = %d\n", bytes_wrote);
     serial_close(hserial);
 
     return 0;
