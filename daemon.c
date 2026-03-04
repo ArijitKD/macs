f292a056967a0e06680243f5fa631ea18e602858
Arijit Kumar Das <arijitkdgit.official@gmail.com>
2026-03-04 23:26:53 +0530

    Update daemon.c to use update_macs_state() function
    Signed-off-by: Arijit Kumar Das <arijitkdgit.official@gmail.com>

--- daemon.c
+++ daemon.c
@@ -1,49 +1,44 @@
 #include <stdio.h>
 #include <string.h>
-#include "serial/port.h"
-#include "serial/io.h"
 #include "serial/macs.h"
 
 
 int main(int argc, char *argv[])
 {
-    uint_t status_on = 1;
+    uint_t state = MACS_STATE_ON;
     if (argc > 1 && strcmp(argv[1], "off") == 0)
     {
-        printf ("New state = OFF\n");
-        status_on = 0;
+        printf("New state = OFF\n");
+        state = MACS_STATE_OFF;
     }
     else if (argc > 1 && strcmp(argv[1], "on") == 0)
     {
-        printf ("New state = ON\n");
+        printf("New state = ON\n");
     }
     else
     {
-        printf ("Bad flag or no flag (only \'on\' or \'off\' accepted).\n");
+        printf("Bad flag or no flag (only \'on\' or \'off\' accepted).\n");
         return 1;
     }
-    serial_properties_t s_properties = { MACS_BAUDRATE, MACS_SERIAL_IO_TIMEOUT_MS };
-    uint_t ports[PORT_MAX] = {0};
-    uint_t active_ports_count = get_active_port_nos(ports, PORT_MAX);
-    int macs_port = get_macs_port_no(ports, active_ports_count, &s_properties);
 
-    if (macs_port == -1)
+    int macs_port, errcode;
+    update_macs_state(state, &macs_port, &errcode);
+
+    switch (errcode)
     {
-        printf ("Error: No MACS device found!!!\nFailed to change state.\n");
-        return 1;
+        case MACS_ERR_NO_DEVICE:
+            printf("Error: No MACS device found!!!\nFailed to change state.\n");
+            return 1;
+        
+        case MACS_ERR_WRITE_FAILURE:
+            printf("MACS device detected at port: COM%d\n", macs_port);
+            printf("Error: Write failure on device!!!\nFailed to change state.\n");
+            return 1;
+        
+        default:
+            printf("MACS device detected at port: COM%d\n", macs_port);
+            printf("Successfully changed state.\n");
     }
-    printf ("MACS device detected at port: COM%d\n", macs_port);
-
-    serial_handle_t hserial = serial_open(macs_port, &s_properties);
-    int bytes_wrote;
-
-    if (status_on)
-        bytes_wrote = serial_write(hserial, MACS_SET_ON, MACS_ON_OFF_WRITE_SIZE);
-    else
-        bytes_wrote = serial_write(hserial, MACS_SET_OFF, MACS_ON_OFF_WRITE_SIZE);
-
-    printf("Bytes wrote = %d\n", bytes_wrote);
-    serial_close(hserial);
-
+ 
     return 0;
 }
